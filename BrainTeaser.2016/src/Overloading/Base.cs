4d38396162c5f48c3c6dddeaeabef00957384c23
suchitkhanna <khanna.suchit@gmail.com>
2016-10-16 10:57:55 +0530

    [BT-0001] Adding a solution folder and first teaser.

---
+++ BrainTeaser.2016/src/Overloading/Base.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    class Base
+    {
+        public virtual void Foo(int x)
+        {
+            Console.WriteLine("Base.Foo(int)");
+        }
+    }
+
+    class Derived : Base
+    {
+        public override void Foo(int x)
+        {
+            Console.WriteLine("Derived.Foo(int)");
+        }
+
+        public void Foo(object o)
+        {
+            Console.WriteLine("Derived.Foo(object)");
+        }
+    }
+}
---
+++ BrainTeaser.2016/src/Overloading/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Derived d = new Derived();
+            int i = 10;
+
+            // What is the output of the following ?
+            d.Foo(i);
+
+            Console.ReadLine();
+        }
+    }
+}
